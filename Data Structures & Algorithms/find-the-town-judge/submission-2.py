9c4d0de2249371fb78ee8b0f07156fb0e36c1992
ShivanshCharak <96943825+ShivanshCharak@users.noreply.github.com>
2026-04-11 11:30:14 +0530

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        judge=trust[0][1]
+        for t in trust:
+            if t[0]==t[1] or t[0] == judge or t[1] != judge:
+                return -1
+            judge = t[1]
+        return judge        
+        
